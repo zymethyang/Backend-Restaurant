eb2f9c4819df2a39ebd861be7a45dae3c6702e5f
Nguyen Minh Tri <zymethyang@gmail.com>
2018-02-18 20:30:28 +0700

    first commit

---
+++ models/leaders.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+var Currency = mongoose.Types.Currency;
+
+var leaderSchema = new Schema({
+    name :{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    designation:{
+        type:String,
+        required:true
+    },
+    abbr:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    featured:{
+        type:Boolean,
+        required:false
+    }
+}, {
+        timestamps: true
+    });
+
+var Leaders = mongoose.model('Leader', leaderSchema);
+module.exports = Leaders;
